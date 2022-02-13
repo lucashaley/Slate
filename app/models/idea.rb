152e54a2870b313be99ae68227860d35e575f07d
Lucas Haley <lucashaley@users.noreply.github.com>
2022-02-13 21:38:11 +1300

    Fixed delete with comments

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ApplicationRecord
   has_and_belongs_to_many :realms
-  has_many :comments, inverse_of: :idea
+  has_many :comments, inverse_of: :idea, dependent: :destroy
 
   accepts_nested_attributes_for :comments, :allow_destroy => true
 
