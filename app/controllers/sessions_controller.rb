0a471868d608dc2e8bd213d87192b53691ed4203
Lucas Haley <lucashaley@users.noreply.github.com>
2022-02-21 22:56:00 +1300

    Added User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    log_in params[:session][:student_number]
-    redirect_to root_path
+    # log_in params[:session][:student_number]
+    @user = User.find_by_student_number(params[:session][:student_number])
+    if !!@user
+      session[:student_number] = params[:session][:student_number]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      message = "Something went wrong!"
+      redirect_to root_path, notice: message
+    end
   end
 
   def destroy
-    log_out
+    # log_out
+    session.delete(:student_number)
+    session.delete(:user_id)
     redirect_to root_url
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,26 @@
 module SessionsHelper
-  def log_in(student_number)
-    session[:student_number] = student_number
-  end
-
-  def log_out
-    session.delete(:student_number)
-    @current_student = nil
-  end
+  # def log_in(student_number)
+  #   session[:student_number] = student_number
+  #   session[:current_user] = User.find_by_student_number(student_number)
+  # end
+  #
+  # def log_out
+  #   session.delete(:student_number)
+  #   session.delete(:current_user)
+  #   @current_student = nil
+  # end
 
   def current_student
     @current_student ||= session[:student_number]
+    # @current_user ||= User.find_by_student_number(session[:student_number]) if !!session[:student_number]
+  end
+
+  def current_user
+    @current_user ||= User.find_by_student_number(session[:student_number]) if !!session[:student_number]
   end
 
   def logged_in?
-    !current_student.nil?
+    # !current_student.nil?
+    !!session[:student_number]
   end
 end
