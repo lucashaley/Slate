1e1b962701c42a5dcef9c2265d4640a4ae4db2e6
Lucas Haley <lucashaley@users.noreply.github.com>
2022-03-03 14:44:51 +1300

    Fixed ratings dependency

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,7 +3,7 @@ class Idea < ApplicationRecord
   # belongs_to :user
   has_and_belongs_to_many :realms
   has_many :comments, inverse_of: :idea, dependent: :destroy
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
 
   accepts_nested_attributes_for :comments, :allow_destroy => true
   accepts_nested_attributes_for :ratings, :allow_destroy => true
